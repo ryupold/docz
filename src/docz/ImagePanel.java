90511d76b27a03a37cab769bf42a5d490a74d5f9
RyU <realriu@gmail.com>
2014-09-29 15:51:48 +0200

    ~ improved imagepanel now it's possible to zoom and drag

--- src/docz/ImagePanel.java
+++ src/docz/ImagePanel.java
@@ -8,18 +8,18 @@ package docz;
 import de.realriu.riulib.helpers.ScaleImage;
 import de.realriu.riulib.helpers.ScaleImage.Rectangle;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Point;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.awt.image.BufferedImage;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
@@ -32,6 +32,9 @@ public class ImagePanel extends JPanel {
     private File imgFile = null;
     private Image scaledImg = null;
     private Rectangle preferedSize;
+    private int camX = 0, camY = 0;
+    private float zoom = 1f;
+    private Point startDrag = null;
 
     public ImagePanel() {
         addComponentListener(new ComponentListener() {
@@ -39,10 +42,9 @@ public class ImagePanel extends JPanel {
             @Override
             public void componentResized(ComponentEvent e) {
                 try {
-                    if(imgFile != null){
+                    if (imgFile != null) {
                         setImg(imgFile);
-                    }
-                    else{
+                    } else {
                         repaint();
                     }
                 } catch (IOException ex) {
@@ -65,6 +67,34 @@ public class ImagePanel extends JPanel {
 
             }
         });
+
+        addMouseWheelListener(new MouseWheelListener() {
+
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                zoom -= e.getPreciseWheelRotation();
+                zoom = Math.max(Math.min(10f, zoom), 1f);
+                repaint();
+            }
+        });
+        
+        
+
+        addMouseMotionListener(new MouseMotionListener() {
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                //Log.l("drag:" + e.getButton());
+                camX = getWidth() - e.getX();
+                camY = getHeight() - e.getY();
+                repaint();
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+
+            }
+        });
     }
 
     public File getImg() {
@@ -74,28 +104,37 @@ public class ImagePanel extends JPanel {
     public Image getScaledImg() {
         return scaledImg;
     }
-    
-    
+
+    private void resetCam() {
+        camX = getWidth() / 2;
+        camY = getHeight() / 2;
+        zoom = 1f;
+    }
 
     public void setImg(File imgFile) throws IOException {
         this.imgFile = imgFile;
         if (imgFile != null) {
             BufferedImage img = ImageIO.read(imgFile);
-            if(img != null){
+            if (img != null) {
                 preferedSize = ScaleImage.fitToRect(new ScaleImage.Rectangle(0, 0, getWidth(), getHeight()), (BufferedImage) img);
                 this.scaledImg = ScaleImage.scale((BufferedImage) img, preferedSize.width, preferedSize.heigth);
-            }else{
+            } else {
                 scaledImg = null;
                 this.imgFile = null;
             }
         }
+
+        resetCam();
+
         repaint();
     }
-    
-    
-    public void setImg(Image img){
+
+    public void setImg(Image img) {
         scaledImg = img;
         preferedSize = ScaleImage.fitToRect(new ScaleImage.Rectangle(0, 0, getWidth(), getHeight()), (BufferedImage) img);
+
+        resetCam();
+
         repaint();
     }
 
@@ -104,7 +143,13 @@ public class ImagePanel extends JPanel {
         g.setColor(Color.black);
         g.fillRect(0, 0, getWidth(), getHeight());
         if (scaledImg != null && getWidth() > 0 && getHeight() > 0) {
-            g.drawImage(scaledImg, preferedSize.x, preferedSize.y, preferedSize.width, preferedSize.heigth, this);
+            Log.l("zoom = " + zoom + "    X = " + camX + "    Y = " + camY);
+            g.drawImage(scaledImg,
+                    /*X*/ (int) (preferedSize.x - (camX * zoom - getWidth() / 2f)),
+                    /*Y*/ (int) (preferedSize.y - (camY * zoom - getHeight() / 2f)),
+                    /*W*/ (int) (preferedSize.width * zoom),
+                    /*H*/ (int) (preferedSize.heigth * zoom),
+                    this);
         }
     }
 
